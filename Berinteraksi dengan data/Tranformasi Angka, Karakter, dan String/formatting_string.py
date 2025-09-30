e82861c1e7863b0090d6379211bbd8187dd787c3
chalik1pj <syafwanchalik5@gmail.com>
2025-09-30 09:14:01 +0700

    Python Learn

---
+++ Berinteraksi dengan data/Tranformasi Angka, Karakter, dan String/formatting_string.py
@@ -0,0 +1,15 @@
+# zfill()
+teks = 'Code'
+tambah_number = teks.zfill(5)
+print(tambah_number)
+
+# rjust()
+print('Dicoding'.rjust(20))
+print('Dicoding'.rjust(20, '!'))
+
+# ljust()
+print('Dicoding'.ljust(20))
+print('Dicoding'.ljust(20, '*'))
+
+# center()
+print('Dicoding'.center(10, '-'))
---
+++ Berinteraksi dengan data/Tranformasi Angka, Karakter, dan String/pengecekan_string.py
@@ -0,0 +1,24 @@
+# isupper()
+kata = 'DICODING'
+print(kata.isupper())
+
+# islower()
+kata = 'dicoding'
+print(kata.islower())
+
+# isalpha()
+kata = 'Dicoding'
+print(kata.isalpha())
+
+# isalnum()
+kata = 'Dicoding123'
+print(kata.isalnum())
+
+# isdecimal()
+print('12345'.isdecimal())
+
+# isspace()
+print('   '.isspace())
+
+# istitle()
+kata = 'Dicoding Indonesia'
